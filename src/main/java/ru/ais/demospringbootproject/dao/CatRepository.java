0a513c6a6a289b68fa132598cd70c47a8765c586
Vyacheslav Lapin <vyacheslav@y-tree.com>
2019-11-06 10:45:34 +0300

    init - 2

--- src/main/java/ru/ais/demospringbootproject/dao/CatRepository.java
+++ src/main/java/ru/ais/demospringbootproject/dao/CatRepository.java
@@ -1,5 +1,10 @@
 package ru.ais.demospringbootproject.dao;
 
-public class CatRepository {
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import ru.ais.demospringbootproject.model.Cat;
 
+@RepositoryRestResource
+public interface CatRepository extends JpaRepository<Cat, UUID> {
 }
--- src/test/java/ru/ais/demospringbootproject/DemoSpringBootProjectApplicationTests.java
+++ src/test/java/ru/ais/demospringbootproject/DemoSpringBootProjectApplicationTests.java
@@ -1,13 +1,59 @@
 package ru.ais.demospringbootproject;
 
+import static com.jayway.jsonpath.JsonPath.parse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.hateoas.MediaTypes.HAL_JSON_VALUE;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.jayway.jsonpath.JsonPath;
+import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import ru.ais.demospringbootproject.dao.CatRepository;
+import ru.ais.demospringbootproject.model.Cat;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+//@WithMockUser(authorities = "ADMIN")
 class DemoSpringBootProjectApplicationTests {
+
+  MockMvc mockMvc;
+
+  CatRepository catRepository;
+
+  @BeforeEach
+  void setUp() {
+    Arrays.stream("Васька,Мурка,Барсик".split(","))
+        .map(Cat::new)
+        .forEach(catRepository::save);
+  }
 
   @Test
   void contextLoads() {
   }
 
+  @Test
+  @SneakyThrows
+  @DisplayName("cats method works correctly")
+  void getCats() {
+    mockMvc.perform(get("/cats"))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(HAL_JSON_VALUE))
+        .andExpect(mvcResult -> assertEquals(3,
+            parse(mvcResult.getResponse().getContentAsString())
+                .<Integer>read("$.page.totalElements").intValue()));
+  }
 }
